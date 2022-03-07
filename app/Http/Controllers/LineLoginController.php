a0f1ad308d2dfeac095f626bcd9ce233e6366f21
shoyah <rio0901dog@outlook.jp>
2022-03-07 14:20:41 +0900

    バグ修正

--- app/Http/Controllers/LineLoginController.php
+++ app/Http/Controllers/LineLoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use App\Models\User;
+use App\User;
 use Illuminate\Support\Facades\Auth;
  
 class LineLoginController extends Controller
